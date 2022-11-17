f015d9f6b18a75415d7e5713036d2a45f96a22d6
Hasan Balcı <balcihasan99@gmail.com>
2022-11-17 17:27:37 +0300

    Add Graph and GraphManager classes

---
+++ src/graph-manager.js
@@ -0,0 +1,281 @@
+/**
+ * This class represents a graph manager for complexity management purposes. A graph
+ * manager maintains a collection of graphs, forming a compound graph structure
+ * through inclusion, and maintains its owner complexity manager, its root graph, 
+ * its inter-graph edges, its sibling graph manager and the information of whether 
+ * it is visible or not.
+ */
+export class GraphManager {
+  // Complexity manager that owns this graph manager
+  #owner;
+
+  // Graphs maintained by this graph manager, including the root of the nesting hierarchy
+  #graphs;
+
+	/*
+	 * Inter-graph edges in this graph manager. Notice that all inter-graph
+	 * edges go here, not in any of the edge lists of individual graphs (either
+	 * source or target node's owner graph).
+	 */  
+  #edges;
+
+  // The root of the inclusion/nesting hierarchy of this compound structure
+  #rootGraph;
+
+  /**
+   * Sibling graph manager of this graph manager. If this graph manager is managing the visible 
+   * graph, then siblingGraphManager manages the invisible graph or vice versa.
+   */  
+  #siblingGraphManager;
+
+  // Whether this graph manager manages the visible graph or not
+  #isVisible;
+
+  /**
+   * 
+   * @param {ComplexityManager} owner - owner complexity manager 
+   * @param {bool} isVisible - whether the graph manager manages visible graph or not
+   */
+  constructor(owner, isVisible) {
+    this.#owner = owner;
+    this.#graphs = [];
+    this.#edges = [];
+    this.#rootGraph = null
+    this.#siblingGraphManager = null;
+    this.#isVisible = isVisible;
+  }
+
+  // get methods
+  get owner() {
+    return this.#owner;
+  }
+
+  get graphs() {
+    return this.#graphs;
+  }
+
+  get edges() {
+    return this.#edges
+  }
+
+  get rootGraph() {
+    return this.#rootGraph;
+  }
+
+  get siblingGraphManager() {
+    return this.#siblingGraphManager;
+  }
+
+  get isVisible() {
+    return this.#isVisible;
+  }
+
+  // set methods
+  set owner(owner) {
+    this.#owner = owner;
+  }
+
+  set graphs(graphs) {
+    this.#graphs = graphs;
+  }
+
+  set edges(edges) {
+    this.#edges = edges;
+  }
+
+  set rootGraph(rootGraph) {
+    if (rootGraph.owner != this) {
+      throw "Root not in this graph mgr!";
+    }
+  
+    this.#rootGraph = rootGraph;
+
+    // root graph must have a root node associated with it for convenience
+    if (rootGraph.parent == null)
+    {
+      rootGraph.parent = this.owner.newNode("Root node");
+    }
+  }
+
+  set siblingGraphManager(siblingGraphManager) {
+    this.#siblingGraphManager = siblingGraphManager;
+  }
+
+  set isVisible(isVisible) {
+    this.#isVisible = isVisible;
+  }
+
+	/**
+	 * This method adds a new graph to this graph manager and sets as the root.
+	 * It also creates the root node as the parent of the root graph.
+	 */  
+  addRoot() {
+    let newGraph = this.#owner.newGraph();
+    let newNode = this.#owner.newNode(null);
+    let root = this.addGraph(newGraph, newNode);
+    this.rootGraph = root;
+    return this.#rootGraph;
+  }
+
+	/**
+	 * This method adds the input graph into this graph manager. The new graph
+	 * is associated as the child graph of the input parent node. If the parent
+	 * node is null, then the graph is set to be the root.
+	 */
+  addGraph(newGraph, parentNode) {
+    if (newGraph == null) {
+      throw "Graph is null!";
+    }
+    if (parentNode == null) {
+      throw "Parent node is null!";
+    }
+    if (this.#graphs.indexOf(newGraph) > -1) {
+      throw "Graph already in this graph mgr!";
+    }
+
+    this.#graphs.push(newGraph);
+
+    if (newGraph.parent != null) {
+      throw "Already has a parent!";
+    }
+    if (parentNode.child != null) {
+      throw  "Already has a child!";
+    }
+
+    newGraph.parent = parentNode;
+    parentNode.child = newGraph;
+
+    return newGraph;
+  }
+
+	/**
+	 * This method adds the input edge between specified nodes into this graph
+	 * manager. We assume both source and target nodes to be already in this
+	 * graph manager.
+	 */
+  addInterGraphEdge(newEdge, sourceNode, targetNode) {
+    const sourceGraph = sourceNode.owner;
+    const targetGraph = targetNode.owner;
+
+    if (!(sourceGraph != null && sourceGraph.getGraphManager() == this)) {
+      throw "Source not in this graph mgr!";
+    }
+    if (!(targetGraph != null && targetGraph.getGraphManager() == this)) {
+      throw "Target not in this graph mgr!";
+    }
+
+    if (sourceGraph == targetGraph) {
+      newEdge.isInterGraph = false;
+      return sourceGraph.add(newEdge, sourceNode, targetNode);
+    }
+    else {
+      newEdge.isInterGraph = true;
+
+      // set source and target
+      newEdge.source = sourceNode;
+      newEdge.target = targetNode;
+
+      // add edge to inter-graph edge list
+      if (this.#edges.indexOf(newEdge) > -1) {
+        throw "Edge already in inter-graph edge list!";
+      }
+
+      this.#edges.push(newEdge);
+
+      // add edge to source and target incidency lists
+      if (!(newEdge.source != null && newEdge.target != null)) {
+        throw "Edge source and/or target is null!";
+      }
+
+      if (!(newEdge.source.edges.indexOf(newEdge) == -1 && newEdge.target.edges.indexOf(newEdge) == -1)) {
+        throw "Edge already in source and/or target incidency list!";
+      }
+
+      newEdge.source.edges.push(newEdge);
+      newEdge.target.edges.push(newEdge);
+
+      return newEdge;
+    }
+  }
+
+  /**
+   * This method removes the input graph from this graph manager. 
+   */
+  removeGraph(graph) {
+    if (graph.getGraphManager() != this) {
+      throw "Graph not in this graph mgr";
+    }
+    if (!(graph == this.rootGraph || (graph.parent != null && graph.parent.graphManager == this))) {
+      throw "Invalid parent node!";
+    }
+
+    // first the edges (make a copy to do it safely)
+    let edgesToBeRemoved = [];
+
+    edgesToBeRemoved = edgesToBeRemoved.concat(graph.edges);
+
+    edgesToBeRemoved.forEach(edge => {
+      graph.remove(edge);
+    });
+
+    // then the nodes (make a copy to do it safely)
+    let nodesToBeRemoved = [];
+
+    nodesToBeRemoved = nodesToBeRemoved.concat(graph.nodes);
+
+    nodesToBeRemoved.forEach(node => {
+      graph.remove(mode);
+    });
+
+    // check if graph is the root
+    if (graph == this.#rootGraph)
+    {
+      this.#rootGraph = null;
+    }
+
+    // now remove the graph itself
+    this.graphs.remove(graph);
+
+    // also reset the parent of the graph
+    graph.parent = null;
+  }
+
+
+	/**
+	 * This method removes the input inter-graph edge from this graph manager.
+	 */
+  removeInterGraphEdge(edge) {
+    if (edge == null) {
+      throw "Edge is null!";
+    }
+    if (!edge.isInterGraph) {
+      throw "Not an inter-graph edge!";
+    }
+    if (!(edge.source != null && edge.target != null)) {
+      throw "Source and/or target is null!";
+    }
+
+    // remove edge from source and target nodes' incidency lists      
+  
+    if (!(edge.source.edges.indexOf(edge) != -1 && edge.target.edges.indexOf(edge) != -1)) {
+      throw "Source and/or target doesn't know this edge!";
+    }
+
+    let index = edge.source.edges.indexOf(edge);
+    edge.source.edges.splice(index, 1);
+    index = edge.target.edges.indexOf(edge);
+    edge.target.edges.splice(index, 1);
+
+    // remove edge from owner graph manager's inter-graph edge list
+
+    if (!(edge.source.owner != null && edge.source.owner.getGraphManager() != null)) {
+      throw "Edge owner graph or owner graph manager is null!";
+    }
+    if (edge.source.owner.getGraphManager().edges.indexOf(edge) == -1) {
+      throw "Not in owner graph manager's edge list!";
+    }
+
+    index = edge.source.owner.getGraphManager().edges.indexOf(edge);
+    edge.source.owner.getGraphManager().edges.splice(index, 1);
+  }
+}
---
+++ src/graph.js
@@ -0,0 +1,226 @@
+/**
+ * This class represents a graph. A graph maintains
+ * its owner graph manager, its nodes, its intra-graph edges,
+ * its parent node and its sibling graph. A graph is always
+ * a child of a compound node. The root of the compound graph
+ * structure is a child of the root node, which is the only node
+ * in compound structure without an owner graph.
+ */
+export class Graph {
+  /** 
+   * Parent node of the graph. This should never be null (the parent of the
+	 * root graph is the root node) when this graph is part of a compound
+   * structure (i.e. a graph manager).
+   */ 
+  #parent;
+
+  // Graph manager that owns this graph
+  #owner;
+
+  // Nodes maintained by the graph
+  #nodes;
+
+  // Edges whose source and target nodes are in this graph (intra-graph edge)
+  #edges;
+
+  /**
+   * Sibling graph of this graph. If this graph is owned by the visible 
+   * graph manager, then siblingGraph must be owned by the invisible
+   * graph manager or vice versa.
+   */
+  #siblingGraph;
+
+  /**
+   * Constructor
+   * @param {Node} parent - parent node of the graph
+   * @param {GraphManager} owner - owner graph manager of the graph
+   */
+  constructor(parent, owner) {
+    this.#parent = parent;
+    this.#owner = owner;
+    this.#nodes = [];
+    this.#edges = [];
+    this.#siblingGraph = null;
+  }
+
+  // get methods
+  get parent() {
+    return this.#parent;
+  }
+
+  get owner() {
+    return this.#owner;
+  }
+
+  get nodes() {
+    return this.#nodes;
+  }
+
+  get edges() {
+    return this.#edges
+  }
+
+  get siblingGraph() {
+    return this.#siblingGraph;
+  }
+
+  // set methods
+  set parent(parent) {
+    this.#parent = parent;
+  }
+
+  set owner(owner) {
+    this.#owner = owner;
+  }
+
+  set nodes(nodes) {
+    this.#nodes = nodes;
+  }
+
+  set edges(edges) {
+    this.#edges = edges;
+  }
+
+  set siblingGraph(siblingGraph) {
+    this.#siblingGraph = siblingGraph;
+  }
+
+	/**
+	 * This methods adds the given node to this graph. We assume 
+   * this graph has a proper graph manager.
+	 */  
+  addNode(newNode) {
+    if (this.#owner == null) {
+      throw "Graph has no graph manager!"
+    }
+
+    if (this.#nodes.indexOf(newNode) > -1) {
+      throw "Node already in graph!"
+    }
+
+    newNode.owner = this;
+    this.#nodes.push(newNode);
+
+    return newNode;
+  }
+
+	/**
+	 * This methods adds the given edge to this graph with 
+   * specified nodes as source and target.
+	 */  
+  addEdge(newEdge, sourceNode, targetNode) {
+    if (!(this.#nodes.indexOf(sourceNode) > -1 && (this.#nodes().indexOf(targetNode)) > -1)) {
+      throw "Source or target not in graph!";
+    }
+
+    if (!(sourceNode.owner == targetNode.owner && sourceNode.owner == this)) {
+      throw "Both owners must be this graph!";
+    }
+
+    if (sourceNode.owner != targetNode.owner)
+    {
+      return null;
+    }
+
+    // set source and target
+    newEdge.source = sourceNode;
+    newEdge.target = targetNode;
+
+    // set as intra-graph edge
+    newEdge.isInterGraph = false;
+
+    // add to graph edge list
+    this.#edges().push(newEdge);
+
+    // add to incidency lists
+    sourceNode.edges.push(newEdge);
+
+    if (targetNode != sourceNode)
+    {
+      targetNode.edges.push(newEdge);
+    }
+
+    return newEdge;
+  }
+
+	/**
+	 * This method removes the input node from this graph. If the node has any
+	 * incident edges, they are removed from the graph (the graph manager for
+	 * inter-graph edges) as well.
+	 */
+  removeNode(node) {
+    if (node == null) {
+      throw "Node is null!";
+    }
+    if (!(node.owner != null && node.owner == this)) {
+      throw "Owner graph is invalid!";
+    }
+    if (this.owner == null) {
+      throw "Owner graph manager is invalid!";
+    }
+
+    // remove incident edges first (make a copy to do it safely)
+    const edgesToBeRemoved = node.edges.slice();
+    let edge;
+    edgesToBeRemoved.forEach(edge => {
+      if (edge.isInterGraph)
+      {
+        this.owner.remove(edge);
+      }
+      else
+      {
+        edge.source.owner.remove(edge);
+      }
+    });
+
+    // now the node itself
+    const index = this.#nodes.indexOf(node);
+    if (index == -1) {
+      throw "Node not in owner node list!";
+    }
+
+    this.nodes.splice(index, 1);    
+  }
+
+	/**
+	 * This method removes the input edge from this graph. 
+   * Should not be used for inter-graph edges.
+	 */  
+  removeEdge(edge) {
+    if (edge == null) {
+      throw "Edge is null!";
+    }
+    if (!(edge.source != null && edge.target != null)) {
+      throw "Source and/or target is null!";
+    }
+    if (!(edge.source.owner != null && edge.target.owner != null &&
+            edge.source.owner == this && edge.target.owner == this)) {
+      throw "Source and/or target owner is invalid!";
+    }
+
+    // remove edge from source and target nodes' incidency lists
+
+    const sourceIndex = edge.source.edges.indexOf(edge);
+    const targetIndex = edge.target.edges.indexOf(edge);
+
+    if (!(sourceIndex > -1 && targetIndex > -1)) {
+      throw "Source and/or target doesn't know this edge!";
+    }
+
+    edge.source.edges.splice(sourceIndex, 1);
+
+    if (edge.target != edge.source)
+    {
+      edge.target.edges.splice(targetIndex, 1);
+    }
+
+    // remove edge from owner graph's edge list
+
+    const index = edge.source.owner.getEdges().indexOf(edge);
+    if (index == -1) {
+      throw "Not in owner's edge list!";
+    }
+
+    edge.source.owner.edges.splice(index, 1);
+  }
+}
